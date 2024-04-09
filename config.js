a729b4398e088d9e0c2b8294cf3fcbc5cfec473a
Darkybot <149830122+Darkybot@users.noreply.github.com>
2024-04-09 13:00:29 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['22898736574', 'darky-bot', true],
+  ['22898736574', 'niggah', true], 
   [''] 
 ] //Number of owners
 
